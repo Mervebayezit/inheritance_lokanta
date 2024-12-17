0170bc3d283bef29d386eae5a793f2ceb2aa8e46
Merve <Merve@MERVE>
2024-12-18 00:05:39 +0300

    Proje dosyası ekle.

---
+++ ntplokanta/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace Lokanta
+{
+    public class Menu
+    {
+        public string Isim { get; set; }
+        public string Cesit { get; set; }
+        public decimal Fiyat { get; set; }
+        public string Aciklama { get; set; }
+
+        public Menu(string isim, string cesit, decimal fiyat, string aciklama)
+        {
+            Isim = isim;
+            Cesit = cesit;
+            Fiyat = fiyat;
+            Aciklama = aciklama;
+        }
+
+        public virtual void BilgiYazdir()
+        {
+            Console.WriteLine($"İsim: " + Isim);
+            Console.WriteLine("Çeşit: " + Cesit);
+            Console.WriteLine("Fiyat: TL " + Fiyat);
+            Console.WriteLine("Açıklama: " + Aciklama);
+        }
+    }
+
+    public class Yemek : Menu
+    {
+        public string Porsiyon { get; set; }
+        public bool Vegan { get; set; }
+
+        public Yemek(string isim, string cesit, decimal fiyat, string aciklama, string porsiyon, bool vegan)
+         : base(isim, cesit, fiyat, aciklama)
+
+        {
+            Porsiyon = porsiyon;
+            Vegan = vegan;
+        }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine("Porsiyon: " + Porsiyon);
+            Console.WriteLine("Vegan: " + Vegan);
+        }
+    }
+
+    public class Kahvalti : Menu
+    {
+        public bool SicakIcecekDahil { get; set; }
+        public bool AcikBufe { get; set; }
+
+        public Kahvalti(string isim, string cesit, decimal fiyat, string aciklama, bool sicakIcecekDahil, bool acikBufe)
+         : base(isim, cesit, fiyat, aciklama)
+
+        {
+            SicakIcecekDahil = sicakIcecekDahil;
+            AcikBufe = acikBufe;
+        }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine("Sıcak İçecek Dahil: " + SicakIcecekDahil);
+            Console.WriteLine("Açık Büfe: " + AcikBufe);
+        }
+    }
+    public class Tatli : Menu
+    {
+        public bool Glutensiz { get; set; }
+        public int Kalori { get; set; }
+
+        public Tatli(string isim, string cesit, decimal fiyat, string aciklama, bool glutensiz, int kalori)
+        : base(isim, cesit, fiyat, aciklama)
+
+        {
+            Glutensiz = glutensiz;
+            Kalori = kalori;
+        }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine("Glutensiz: " + Glutensiz);
+            Console.WriteLine("Kalori: kcal " + Kalori);
+        }
+    }
+
+    public class Icecek : Menu
+    {
+        public bool Alkollu { get; set; }
+        public string Boyut { get; set; }
+
+        public Icecek(string isim, string cesit, decimal fiyat, string aciklama, bool alkollu, string boyut)
+          : base(isim, cesit, fiyat, aciklama)
+        {
+            Alkollu = alkollu;
+            Boyut = boyut;
+        }
+
+        public override void BilgiYazdir()
+        {
+            base.BilgiYazdir();
+            Console.WriteLine("Alkollü: " + Alkollu);
+            Console.WriteLine("Boyut: " + Boyut);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Yemek yemek = new Yemek("cağ kebabı", "Ana Yemek", 45.00m, "Baharatlı ve közlenmiş sebzelerle servis edilir.", "Tam Porsiyon", false);
+
+            Kahvalti kahvalti = new Kahvalti("van  Kahvaltısı", "Kahvaltı", 65.00m, "Çeşitli peynirler, zeytinler, kavurmalı yumurta, reçeller ve semaverde çay.", true, true);
+
+            Tatli tatli = new Tatli("Güveç Baklava", "Tatlı", 30.00m, "Cevizli ve şerbetli  tatlı.", false, 300);
+
+            Icecek icecek = new Icecek("Ayran", "İçecek", 12.00m, "Yoğurt ve su karışımı ferahlatıcı içecek.", false, "Orta Boy");
+
+            yemek.BilgiYazdir();
+            kahvalti.BilgiYazdir();
+            tatli.BilgiYazdir();
+            icecek.BilgiYazdir();
+        }
+    }
+}
---
+++ ntplokanta/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ntplokanta")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ntplokanta")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("8de94291-34a3-4e8d-b8f3-ce2bf44f1964")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
